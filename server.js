06c1d9930edf7b62acf24986fdf96bf60e9f741f
neelb99 <neelbajaj1234@hotmail.com>
2019-09-25 19:45:20 +0530

    Initial Deploy Setup

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require('path');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.DB_URI, {useCreateIndex:true, useNewUrlParser:true, useUnifiedTopology:true})
+    .then(()=>console.log("DB Connected"))
+    .catch(err=>console.log(err));
+
+if(PORT!==5000){
+    app.use(express.static(path.join(__dirname,"client","build")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"client","build","index.html"));
+    });
+}    
+
+app.listen(PORT,()=>console.log("Server Running"));
